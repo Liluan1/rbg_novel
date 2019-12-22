5d52bd8a3189d4d54a639ef0e61e53a9f2c70762
Liluan <37143470+Liluan1@users.noreply.github.com>
2019-12-22 15:06:00 +0800

    Complete core functions

--- config.py
+++ config.py
@@ -63,6 +63,7 @@ RULES = {
     # content_url=1 表示章节链接使用本身自带的链接，不用拼接
     # content_url=0 表示章节网页需要当前页面url拼接
     # content_url=netloc 表示使用域名进行拼接
+    # example
     # 'www.biqule.com': Rules('www.biqule.com', {'class': 'box_con'},{}),
     # 'www.lingdiankanshu.com': Rules('www.lingdiankanshu.com', {'class': 'box_con'}, {}),
     # 'www.hhlwx.com': Rules('www.hhlwx.co', {'class': 'chapterlist'},{}),
@@ -447,7 +448,7 @@ RULES = {
     # 已解析
     'www.kanshuzhe.com': Rules('http://www.kanshuzhe.com/', {'class': 'box_con'}, {'id': 'content'}),
     # 已解析
-    'www.biqiuge.com': Rules('0', {'class': 'box_con'}, {'id': 'content'}),
+    'www.biqiuge.com': Rules('0', {'class': 'listmain'}, {'id': 'content'}),
     # 已解析
     'www.lwxs.com': Rules('0', {'class': 'box_con'}, {'id': 'TXT'}),
     # 已解析
